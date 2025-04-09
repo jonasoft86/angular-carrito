09ca5350c61a843d803c1a76de19c8cbd07dfdb5
jonasoft86 <jonathan.monroy@live.com>
2025-04-09 09:55:35 -0400

    add route url

--- src/app/components/cart-app.component.ts
+++ src/app/components/cart-app.component.ts
@@ -4,11 +4,12 @@ import { Product } from '../models/product';
 import { CatalogComponent } from './catalog/catalog.component';
 import { CartItem } from '../models/cartItem';
 import { NavbarComponent } from './navbar/navbar.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'cart-app',
   standalone: true,
-  imports: [CatalogComponent, NavbarComponent],
+  imports: [CatalogComponent, NavbarComponent, RouterOutlet],
   templateUrl: './cart-app.component.html'
 })
 export class CartAppComponent implements OnInit {
